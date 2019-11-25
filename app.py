e994e95e2f81d5c797847bb16402c6af3b86ac62
jumeza <jmellado@gmail.com>
2019-11-25 13:29:23 +0100

    testing pipeline2

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    provider = str(os.environ.get('PROVIDER', 'world3'))
+    provider = str(os.environ.get('PROVIDER', 'world4'))
     return 'Hello '+provider+'!'
 
 if __name__ == '__main__':
